8e5dbecd13c712718aa16f65f8d5ce302a806fd5
Belén Garrido <belengl98@hotmail.com>
2018-11-10 19:09:03 +0100

    Relación entre Handy Worker y Application

--- L03 Deliverable - Persistence Models/Proyectos/BELEN/Acme-HandyWork/src/main/java/domain/HandyWorker.java
+++ L03 Deliverable - Persistence Models/Proyectos/BELEN/Acme-HandyWork/src/main/java/domain/HandyWorker.java
@@ -1,15 +1,30 @@
 
 package domain;
 
+import java.util.Collection;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
 
 @Entity
 @Access(AccessType.PROPERTY)
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class HandyWorker extends Endorsable {
+	private Collection<Application> application;
+	
+	@OneToMany
+	public Collection<Application> getApplication() {
+		return application;
+	}
+
+	public void setApplication(Collection<Application> application) {
+		this.application = application;
+	}
+
+	
 
 }
