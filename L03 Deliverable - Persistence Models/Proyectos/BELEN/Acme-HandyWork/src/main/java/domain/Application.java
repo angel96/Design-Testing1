691ef27e400391a37ad8be40af53eefe2232c08f
Belén Garrido <belengl98@hotmail.com>
2018-11-10 18:15:53 +0100

    Corrección de modelos UML y subida de mi parte del proyecto

---
+++ L03 Deliverable - Persistence Models/Proyectos/BELEN/Acme-HandyWork/src/main/java/domain/Application.java
@@ -0,0 +1,69 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Access(AccessType.PROPERTY)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Application extends DomainEntity {
+
+	private Date		moment;
+	private double		offeredPrice;
+	private String		comments;
+	private String		status;
+	private FixUpTask	fixUpTask;
+
+
+	@Temporal(TemporalType.DATE)
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+
+	public double getOfferedPrice() {
+		return this.offeredPrice;
+	}
+
+	public void setOfferedPrice(final double offeredPrice) {
+		this.offeredPrice = offeredPrice;
+	}
+	@NotBlank
+	public String getComments() {
+		return this.comments;
+	}
+
+	public void setComments(final String comments) {
+		this.comments = comments;
+	}
+	@NotBlank
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(final String status) {
+		this.status = status;
+	}
+	@OneToOne
+	public FixUpTask getFixUpTask() {
+		return this.fixUpTask;
+	}
+
+	public void setFixUpTask(final FixUpTask fixUpTask) {
+		this.fixUpTask = fixUpTask;
+	}
+}
---
+++ L03 Deliverable - Persistence Models/Proyectos/BELEN/Acme-HandyWork/src/main/java/domain/Customer.java
@@ -0,0 +1,15 @@
+
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Access(AccessType.PROPERTY)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Customer extends Endorsable {
+
+}
