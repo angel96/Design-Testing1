77dac5bcd999d548d599019df037296135c0b22c
Ezequiel <ezequiel6a@gmail.com>
2018-12-28 00:25:12 +0100

    Convertersde mi parte (zeki)

--- L06 Deliverable - Controllers/Proyecto/Zeki/Acme-HandyWork/src/main/java/converters/FinderToStringConverter.java
+++ L06 Deliverable - Controllers/Proyecto/Zeki/Acme-HandyWork/src/main/java/converters/FinderToStringConverter.java
@@ -20,7 +20,6 @@ public class FinderToStringConverter implements Converter<Finder, String> {
 			result = null;
 		else
 			result = String.valueOf(finder.getId());
-		System.out.println(result);
 
 		return result;
 	}
